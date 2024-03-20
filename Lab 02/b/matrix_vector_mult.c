d392dba197d0a79446d4af776c0333f09bb4d6c9
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-03-20 05:59:45 -0300

    alt

---
+++ Lab 02/b/matrix_vector_mult.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define M 4000 // Número total de linhas da matriz
+#define N 4000 // Número de colunas da matriz e tamanho do vetor
+
+int matrix[M][N];
+int vector[N];
+int result[M] = {0};
+
+typedef struct {
+    int startRow;
+    int endRow;
+} ThreadData;
+
+
+void printMatrix() {
+    printf("Matriz:\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+        if (i == 20) {
+            printf("(matriz muito grande)\n");
+            break;
+        }
+    }
+}
+
+
+void printVector() {
+    printf("Vetor:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", vector[i]);
+        if (i == 20) {
+            printf("(vetor muito grande)\n");
+            break;
+        }
+    }
+    printf("\n");
+}
+
+void* computeRange(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    for (int i = data->startRow; i <= data->endRow; i++) {
+        for (int j = 0; j < N; j++) {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <numero de threads>\n", argv[0]);
+        return 1;
+    }
+    int numThreads = atoi(argv[1]);
+    if (numThreads <= 0) {
+        printf("Número de threads deve ser maior que 0.\n");
+        return 1;
+    }
+    if (numThreads > M) numThreads = M;
+
+    int value = 0;
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < N; j++) {
+            matrix[i][j] = value++;
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        vector[i] = (i % 2 == 0) ? 1 : -1;
+    }
+
+    // printMatrix();
+    // printVector();
+
+    pthread_t threads[numThreads];
+    ThreadData threadData[numThreads];
+
+    int rowsPerThread = M / numThreads;
+    int extraRows = M % numThreads;
+
+    int currentRow = 0;
+    for (int i = 0; i < numThreads; i++) {
+        threadData[i].startRow = currentRow;
+        currentRow += rowsPerThread + (extraRows > 0 ? 1 : 0); // Distribui as linhas
+        threadData[i].endRow = currentRow - 1;
+        extraRows--;
+
+        if (pthread_create(&threads[i], NULL, computeRange, (void*)&threadData[i])) {
+            printf("Erro ao criar thread\n");
+            exit(-1);
+        }
+    }
+
+    for (int i = 0; i < numThreads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Vetor resultante:\n");
+    for (int i = 0; i < M; i++) {
+        printf("%d\n", result[i]);
+    }
+
+    return 0;
+}
